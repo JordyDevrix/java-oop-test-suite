eec150e2101883b21b43ddbb550083bb698f944a
Jordy Devrix <2109jordy2241@gmail.com>
2024-02-09 16:13:22 +0100

    Tested error handling in Java (try-catch_finally)

--- src/bestandLezen/Main.java
+++ src/bestandLezen/Main.java
@@ -15,18 +15,30 @@ public class Main {
             while (input.hasNext()) {
                 System.out.println(input.nextLine());
             }
+            input.close();
         } catch (FileNotFoundException e) {
             System.out.println("bestand niet gevonden");
         }
 
+        FileWriter fileWriter = null;
         try {
-            FileWriter fileWriter = new FileWriter("src/bestandLezen/helloworld.txt", false);
+            fileWriter = new FileWriter("src/bestandLezen/helloworld.txt", false);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
             for (int fois = 0; fois < 10; fois++) {
                 fileWriter.append("hello\n");
             }
-            fileWriter.close();
         } catch (IOException e) {
             System.out.println("bestand niet gevonden");
+        } finally {
+            try {
+                fileWriter.close();
+            } catch (IOException e) {
+                System.out.println("printen niet gelukt");
+            }
         }
     }
 
