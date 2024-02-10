5cdf38524295a7d0f83ac81f80386657f4b2fb76
Jordy Devrix <2109jordy2241@gmail.com>
2024-02-10 01:50:58 +0100

    added whileloop to make shopping cart experience continious

--- src/kassaSysteem/Main.java
+++ src/kassaSysteem/Main.java
@@ -25,70 +25,83 @@ public class Main {
         // Creating shoppingcarts list
         ArrayList<WinkelWagen> winkelWagens = new ArrayList<WinkelWagen>();
 
+        boolean running = true;
+
         // Creating my own shopping cart and giving it a name
         WinkelWagen mijnWagen = new WinkelWagen("Devrix");
 
-        // Creating scanner
-        Scanner input = new Scanner(System.in);
+        // creating a while loop so that the program keeps running
+        while (running) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.println(
+                    """
+                            Make a choice
+                            [1] view shopping cart content
+                            [2] add product
+                            [3] remove product
+                            [4] view all the barcodes in my shopping cart
+                            [5] stop
+                            """
+            );
+
+            int choice = input.nextInt();
+
+            if (Objects.equals(choice, 1)) {
+
+                // SHOWING SHOPPINGCART BEFORE REMOVING CONTENTS
+                // First forloop going through all my products
+                int indx1 = 0;
+                for (String product : mijnWagen.viewWinkelWagen()) {
+                    int indx2 = 0;
+                    /* second forloop going through each product that's available and then checking if it is
+                       inside my shoppingcart
+                     */
+                    for (Product item : products) {
+                        // For debugging purposes...
+                        // System.out.println(indx2 + " " + item.getID() + product);
+                        if (Objects.equals(item.getProductBarcode(), product)) {
+                            System.out.println(products.get(indx2).getProductData());
+                        }
+                        indx2++;
+                    }
+                    indx1++;
+                }
+
+            } else if (Objects.equals(choice, 2)) {
+
+                // adding product
+                Scanner scanInput = new Scanner(System.in);
+                String barcode = scanInput.nextLine();
+                mijnWagen.addProduct(barcode);
+
+            } else if (Objects.equals(choice, 3)) {
+
+                // removing product
+                Scanner scanInput = new Scanner(System.in);
+                String barcode = scanInput.nextLine();
+                mijnWagen.removeProduct(barcode);
+
+            } else if (Objects.equals(choice, 4)) {
+
+                // Checking the barcodes inside my shopping cart
+                System.out.println(mijnWagen.viewWinkelWagen());
+
+            } else if (Objects.equals(choice, 5)) {
+
+                running = false;
+
+            } else {
+
+                System.out.println("this is not an option, please try again");
+
+            }
+        }
 
         // Adding 3 random products to my own shoppingcart
-        mijnWagen.addProduct(productOne.getProductBarcode());   // 1
+        /*mijnWagen.addProduct(productOne.getProductBarcode());   // 1
         mijnWagen.addProduct(productFour.getProductBarcode());  // 2
-        mijnWagen.addProduct(productFour.getProductBarcode());  // 3
+        mijnWagen.addProduct(productFour.getProductBarcode());  // 3*/
 
-        // Adding 1st custom product
-        String barcode1 = input.nextLine();
-        mijnWagen.addProduct(barcode1);  // 1
-
-        // Adding 2nd custom product
-        String barcode2 = input.nextLine();
-        mijnWagen.addProduct(barcode2);  // 2
-
-        // Checking the barcodes inside my shopping cart
-        System.out.println(mijnWagen.viewWinkelWagen());
-
-
-        // SHOWING SHOPPINGCART BEFORE REMOVING CONTENTS
-        // First forloop going through all my products
-        int indx1 = 0;
-        for (String product : mijnWagen.viewWinkelWagen()) {
-            int indx2 = 0;
-            /* second forloop going through each product that's available and then checking if it is
-               inside my shoppingcart
-             */
-            for (Product item : products) {
-                // For debugging purposes...
-                // System.out.println(indx2 + " " + item.getID() + product);
-                if (Objects.equals(item.getProductBarcode(), product)) {
-                    System.out.println(products.get(indx2).getProductData());
-                }
-                indx2++;
-            }
-            indx1++;
-        }
-
-        // Removing 1 product from basket
-        String productToRemove = "8718452112233";
-        System.out.printf("\nremoving product: %s \n\n", productToRemove);
-        mijnWagen.removeProduct(productToRemove);
-
-        // SHOWING SHOPPINGCART AFTER REMOVING CONTENTS
-        // First forloop going through all my products
-        indx1 = 0;
-        for (String product : mijnWagen.viewWinkelWagen()) {
-            int indx2 = 0;
-            /* second forloop going through each product that's available and then checking if it is
-               inside my shoppingcart
-             */
-            for (Product item : products) {
-                // For debugging purposes...
-                // System.out.println(indx2 + " " + item.getID() + product);
-                if (Objects.equals(item.getProductBarcode(), product)) {
-                    System.out.println(products.get(indx2).getProductData());
-                }
-                indx2++;
-            }
-            indx1++;
-        }
     }
 }
