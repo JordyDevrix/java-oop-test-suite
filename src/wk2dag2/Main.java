ba286dabb64aac18e0c2531f4aea83359fb6242e
JordyDevrix <2109jordy2241@gmail.com>
2024-02-15 00:36:26 +0100

    new commit

---
+++ src/wk2dag2/Main.java
@@ -0,0 +1,9 @@
+package wk2dag2;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Model myCar = new Model("f150", "ford");
+        System.out.println(myCar.getSound());
+    }
+}
---
+++ src/wk2dag2/Model.java
@@ -0,0 +1,10 @@
+package wk2dag2;
+
+public class Model extends CarBrand {
+    private String model;
+
+    public Model(String model, String brand) {
+        super(brand);
+        this.model = model;
+    }
+}
