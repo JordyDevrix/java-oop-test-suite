05220bd54ea2cc90b061f2d87487622a546f0c0e
Jordy Devrix <2109jordy2241@gmail.com>
2024-02-23 17:36:33 +0100

    added commentary

--- src/vidcolSuperKlas/Employee.java
+++ src/vidcolSuperKlas/Employee.java
@@ -1,5 +1,12 @@
 package vidcolSuperKlas;
 
+
+/*
+    Employee class is a super class that provides information for all other classes,
+    that share a relation between the super class.
+
+    A superclass cannot be deleted else the relational class will not receive its information
+*/
 public class Employee {
     private int id;
     private String name;
