1565ad1938ab2dafdf727203b4826c9af7292ce3
Jordy Devrix <2109jordy2241@gmail.com>
2024-02-27 18:40:43 +0100

    little testing with classes

--- src/vidcolSuperKlas/Main.java
+++ src/vidcolSuperKlas/Main.java
@@ -9,6 +9,11 @@ public class Main {
 
         Manager devrix = new Manager(4132, "ma men", "IT");
         devrix.executeTask();
+
+        Manager jordy = new Manager(2109, "JORDY DEVRIXXX", "Customer support");
+        System.out.println(jordy.getId());
+
+
     }
 
 }
